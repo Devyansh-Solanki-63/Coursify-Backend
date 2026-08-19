fe8a3aab0448a421417b36d0f1a9c52134458d4d
Devyansh Solanki <panchaldevyansh632003@gmail.com>
2026-08-19 09:23:11 +0530

    changed routes path for swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,16 +1,14 @@
 import "dotenv/config";
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+import path from "path";
 
 // Read dynamic URL or fallback to defaults based on NODE_ENV
 const server_url = process.env.PROD === 'true'
   ? process.env.SERVER_URL
   : process.env.SERVER_LOCAL_URL;
+
+const docsPath = path.resolve(process.cwd(), 'swagger-docs', '*.yaml').replace(/\\/g, '/');
 
 const options = {
   definition: {
@@ -43,7 +41,7 @@ const options = {
       },
     },
   },
-  apis: [path.join(dirname, '../swagger-docs/*.yaml')],
+  apis: [docsPath],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
